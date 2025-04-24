54026f8fc6b07e495869debe9c4a3461a25de6df
root <luishidalgoa@outlook.es>
2025-04-24 21:27:59 +0000

    Launch 0.2 Fixed Music bugs

--- Microservicios/Spring_project/pom_version.sh
+++ Microservicios/Spring_project/pom_version.sh
@@ -1,6 +1,6 @@
 #bin/bash
 
-mvn versions:set versions:commit -DnewVersion="0.1.1"
+mvn versions:set versions:commit -DnewVersion="0.2"
 
 #Comando para hacer ejecutable el script
 # chmod +x pom_version.sh
